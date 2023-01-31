b013853bf26c7692d1cd75ad962c1a0857b923d4
hdj <hdi@guigu.com>
2023-01-31 09:28:39 +0800

    first commit

---
+++ src/main/java/com/hdj/git_demo.java
@@ -0,0 +1,14 @@
+package com.hdj;
+
+import com.sun.javaws.IconUtil;
+
+/**
+ * @version 1.0
+ * @author: HuangDianJun
+ */
+
+public class git_demo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
